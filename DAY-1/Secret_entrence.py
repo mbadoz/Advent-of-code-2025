daa007a6633d0a85ca646641a36b3a8e56cfb4f7
mbadoz <marius.badoz@efrei.net>
2025-12-02 18:43:28 +0000

    part 1

--- DAY-1/Secret_entrence.py
+++ DAY-1/Secret_entrence.py
@@ -3,11 +3,15 @@ with open("DAY-1/input.txt", "r", encoding="utf-8") as f:
     input_text = input_text.replace('L','-')
     input_text = input_text.replace('R','')
     input_lines = input_text.splitlines()
-dial = 0
+dial = 50
 nb_zero = 0
 print("Méthode with dial adjustment")
 for line in input_lines:
-    dial += int(line)%100
+    if int(line) >= 0:
+        dial += int(line)%100
+    else:
+        dial += (int(line)%100)-100
+    
     if dial < 0:
         dial += 100
     elif dial >= 100:
@@ -21,3 +25,4 @@ for line in input_lines:
     if dial == 0:
         nb_zero += 1
 print(nb_zero)
+
