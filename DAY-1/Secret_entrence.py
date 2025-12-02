34114bc38e1c9fc03a9956c2b628023a2f69341b
mbadoz <marius.badoz@efrei.net>
2025-12-02 18:26:25 +0000

    m

---
+++ DAY-1/Secret_entrence.py
@@ -0,0 +1,23 @@
+with open("DAY-1/input.txt", "r", encoding="utf-8") as f:
+    input_text = f.read()
+    input_text = input_text.replace('L','-')
+    input_text = input_text.replace('R','')
+    input_lines = input_text.splitlines()
+dial = 0
+nb_zero = 0
+print("Méthode with dial adjustment")
+for line in input_lines:
+    dial += int(line)%100
+    if dial < 0:
+        dial += 100
+    elif dial >= 100:
+        dial -= 100
+    if dial == 0:
+        nb_zero += 1
+print(nb_zero)
+print("Méthode without dial adjustment")
+for line in input_lines:
+    dial += int(line)%100
+    if dial == 0:
+        nb_zero += 1
+print(nb_zero)
